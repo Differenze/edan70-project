f530c2bb0129f5167b2817231680ad89679b8d41
Lasse Heemann <dat14lhe@student.lu.se>
2018-12-17 17:00:51 +0100

    added statistic printout to benchmark.sh

--- test/benchmark.sh
+++ test/benchmark.sh
@@ -12,16 +12,27 @@ fi
 echo "==== ${FILE1}: ===="
 # genreates FILE1_sorted.dot
 ./test/sort_graph.sh ${FILE1}
-./pacopt/pacopt.bin ${ARGS} ${FILE1}_sorted.dot ${FILE1}_sorted_pacopt.dot
+OUTPUT1="$(./pacopt/pacopt.bin ${ARGS} ${FILE1}_sorted.dot ${FILE1}_sorted_pacopt.dot)"
+echo "$OUTPUT1"
 echo "\n"
+AREA1=$(echo "$OUTPUT1" | grep area | sed 's/[^0-9]*//g')
+
 
 echo "==== ${FILE2}: ===="
 # genreates FILE2_sorted.dot
 ./test/sort_graph.sh ${FILE2}
-./pacopt/pacopt.bin ${ARGS} ${FILE2}_sorted.dot ${FILE2}_sorted_pacopt.dot
+OUTPUT2="$(./pacopt/pacopt.bin ${ARGS} ${FILE2}_sorted.dot ${FILE2}_sorted_pacopt.dot)"
+echo "$OUTPUT2"
 echo "\n"
+AREA2=$(echo "$OUTPUT2" | grep area | sed 's/[^0-9]*//g')
 
 echo "created: ${FILE1}_sorted.dot"
 echo "created: ${FILE2}_sorted.dot"
 echo "created: ${FILE1}_sorted_pacopt.dot"
 echo "created: ${FILE2}_sorted_pacopt.dot"
+
+echo "DELTA:"
+DIF=$(($AREA2-$AREA1))
+echo $DIF
+#bc -l <<< "($AREA2-$AREA1)/$AREA2"
+echo "($AREA2-$AREA1)/$AREA2" | bc -l
